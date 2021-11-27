fe5c58478e634c28d7324ebd71b5cc239fe00674
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-27 14:18:53 +0100

    add some middleware handler error

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+//async errors
+
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorMiddleware = require('./middlewares/error-handler');
+
+//express middleware
+
+app.use(express.json());
+
 const port = 8080;
 
 app.get('/', (req, res) => {
