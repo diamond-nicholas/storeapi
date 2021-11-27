af9c0c08d013bd73734d14e60fb3b74a6f6ac713
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-27 14:27:57 +0100

    setup basic express server

--- app.js
+++ app.js
@@ -6,17 +6,30 @@ require('dotenv').config();
 
 const notFoundMiddleware = require('./middlewares/not-found');
 const errorMiddleware = require('./middlewares/error-handler');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 //express middleware
 
 app.use(express.json());
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('this works live now');
 });
 
-app.listen(port, () => {
-  console.log(`app is listening on port ${port}`);
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, () => {
+      console.log(`app is listening on port ${port}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
