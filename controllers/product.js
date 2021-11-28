7e217800a4f90f224e5650504a6fc79c441e2df1
diamond-nicholas <diamondnicholas154@gmail.com>
2021-11-28 04:35:39 +0100

    add filter by field logic

--- controllers/product.js
+++ controllers/product.js
@@ -8,7 +8,7 @@ const getAllProductStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, field } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -20,11 +20,17 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
   let result = Product.find(queryObject);
+  //sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+  //fields
+  if (field) {
+    const fieldList = field.split(',').join(' ');
+    result = result.select(fieldList);
   }
 
   const products = await result;
